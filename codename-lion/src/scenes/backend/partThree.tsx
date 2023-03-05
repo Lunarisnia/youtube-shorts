0d2413d6bdfceb4ef051146abdf9b56d5a18e15a
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2023-03-05 18:24:24 +0700

    feat(lion): finished part two and three

--- codename-lion/src/scenes/backend/partThree.tsx
+++ codename-lion/src/scenes/backend/partThree.tsx
@@ -1,18 +1,133 @@
+import { Image, Node, Rect } from "@motion-canvas/2d/lib/components";
 import { makeScene2D } from "@motion-canvas/2d/lib/scenes";
-import { waitFor } from "@motion-canvas/core/lib/flow";
-import { createRef } from "@motion-canvas/core/lib/utils";
+import { all, waitFor } from "@motion-canvas/core/lib/flow";
+import { createRef, useDuration } from "@motion-canvas/core/lib/utils";
+import { JetbrainText } from "../../components/jetbrainText";
 import { TechNumber } from "../../components/techNumber";
+
+import tf from "../../images/tf.png";
+import pytorch from "../../images/pytorch.png";
+import numpy from "../../images/numpy.png";
+import gpt from "../../images/chatgpt.png";
+
+import cryptograph from "../../images/cryptograph.svg";
+import hacker from "../../images/hacker.png";
+
+import database from "../../images/database.svg";
+import wifi from "../../images/wifi.svg";
 
 export default makeScene2D(function* (view) {
   // Create your animations here
   const opening = createRef<TechNumber>();
+  const timer = createRef<JetbrainText>();
+
+  const tfIcon = createRef<Image>();
+  const pytorchIcon = createRef<Rect>();
+  const numpyIcon = createRef<Image>();
+  const gptIcon = createRef<Image>();
+  const dataScienceText = createRef<JetbrainText>();
+  const textContainer = createRef<Rect>();
+
+  const cryptoIcon = createRef<Image>();
+  const hackerIcon = createRef<Image>();
+
+  const databaseIcon = createRef<Image>();
+  const wifiIcon = createRef<Image>();
   view.add(
-    <TechNumber
-      ref={opening}
-      chapterTextNumber={"3"}
-      sub1={"CARA MENAMAKAN VARIABEL"}
-    />
+    <>
+      <TechNumber
+        ref={opening}
+        chapterTextNumber={"3"}
+        sub1={"RANAHNYA LUAS!"}
+      />
+      <JetbrainText text={`00:39`} fontSize={250} y={1200} ref={timer} />
+
+      <Image ref={tfIcon} src={tf} height={350} y={1100} />
+      <Rect
+        ref={pytorchIcon}
+        fill={"#3a3a3a"}
+        scale={0.8}
+        radius={30}
+        width={900}
+        height={300}
+        y={1100}
+      >
+        <Image src={pytorch} height={200} />
+      </Rect>
+      <Image ref={numpyIcon} src={numpy} height={300} y={1100} />
+      <Image ref={gptIcon} src={gpt} height={300} y={1110} />
+      <Rect
+        zIndex={5}
+        ref={textContainer}
+        fill={"#606060"}
+        scale={1}
+        radius={30}
+        width={900}
+        height={400}
+        y={1200}
+      >
+        <JetbrainText
+          ref={dataScienceText}
+          text={"DATA SCIENCE"}
+          fontSize={100}
+        />
+      </Rect>
+
+      <Image ref={cryptoIcon} src={cryptograph} height={400} y={1200} />
+      <Image ref={hackerIcon} src={hacker} height={500} y={1200} />
+
+
+      <Image ref={databaseIcon} src={database} x={20} height={600} y={1200} />
+      <Image ref={wifiIcon} src={wifi} x={20} height={400} y={1200} />
+    </>
   );
   yield* opening().animate(1);
-  yield* waitFor(10);
+  yield* waitFor(useDuration("waitFrame"));
+  yield* timer().position.y(0, 0);
+  yield* waitFor(1);
+  yield* timer().text("00:40", 0);
+  yield* waitFor(1);
+  yield* timer().text("00:41", 0);
+  yield* waitFor(1);
+  yield* all(timer().text("00:42", 0), timer().position.x(1200, 1));
+  yield* all(
+    tfIcon().position.y(-200, 1),
+    pytorchIcon().position.y(100, 1),
+    numpyIcon().position.y(400, 1),
+    gptIcon().position.y(700, 1)
+  );
+  yield* waitFor(0.5);
+  yield* all(
+    tfIcon().position.y(1200, 1),
+    pytorchIcon().position.y(1200, 1),
+    numpyIcon().position.y(1200, 1),
+    gptIcon().position.y(1200, 1)
+  );
+  yield* textContainer().position.y(0, 0);
+  yield* waitFor(0.5);
+  yield* textContainer().position.y(1200, 0);
+  yield* dataScienceText().text("SECURITY\nENGINEER", 0);
+  yield* all(cryptoIcon().position.y(0, 1), hackerIcon().position.y(600, 1));
+  yield* waitFor(1);
+  yield* textContainer().position.y(0, 0);
+  yield* waitFor(1);
+  yield* textContainer().position.y(1200, 0);
+  yield* all(
+    cryptoIcon().position.y(1200, 0.5),
+    hackerIcon().position.y(1200, 0.5)
+  );
+  yield* all(
+    databaseIcon().position.y(0, 0.5),
+    wifiIcon().position.y(500, 0.5)
+  );
+  yield* dataScienceText().text("NETWORK\nENGINEER", 0);
+  yield* waitFor(1);
+  yield* textContainer().position.y(0, 0);
+  yield* waitFor(1);
+  yield* textContainer().position.y(1200, 0);
+  yield* all(
+    databaseIcon().position.y(1200, 0.5),
+    wifiIcon().position.y(1200, 0.5)
+  );
+  yield* waitFor(1);
 });
--- codename-lion/src/scenes/backend/partTwo.tsx
+++ codename-lion/src/scenes/backend/partTwo.tsx
@@ -1,4 +1,12 @@
-import { CodeBlock, Image } from "@motion-canvas/2d/lib/components";
+import {
+  CodeBlock,
+  edit,
+  Image,
+  insert,
+  Layout,
+  lines,
+  Node,
+} from "@motion-canvas/2d/lib/components";
 import { makeScene2D } from "@motion-canvas/2d/lib/scenes";
 import { all, waitFor } from "@motion-canvas/core/lib/flow";
 import { createRef } from "@motion-canvas/core/lib/utils";
@@ -6,6 +14,7 @@ import { TechNumber } from "../../components/techNumber";
 
 import databaseIcon from "../../images/database.svg";
 import chatgptIcon from "../../images/chatgpt.png";
+import { JetbrainText } from "../../components/jetbrainText";
 
 export default makeScene2D(function* (view) {
   // Create your animations here
@@ -16,6 +25,13 @@ export default makeScene2D(function* (view) {
   // TODO: add text for the task to create a loop to display 1-3
   // Then show this code and the wrong input, make some question mark popup
   // then fix the damn code, sync it with "atensi kepada detil2 kecil"
+
+  const questionContainer = createRef<Node>();
+  const outputContainer = createRef<Layout>();
+  const wrongCode = createRef<CodeBlock>();
+
+  const qMark1 = createRef<JetbrainText>();
+  const qMark2 = createRef<JetbrainText>();
   view.add(
     <>
       <TechNumber
@@ -24,10 +40,35 @@ export default makeScene2D(function* (view) {
         sub1={"LOGIC SKILL TERASAH"}
       />
 
-      <Image position={[30, 0]} ref={database} height={600} src={databaseIcon} />
-      <Image position={[1200, 1200]} ref={chatgpt} height={600} src={chatgptIcon} />
+      <Image
+        position={[30, 0]}
+        ref={database}
+        height={600}
+        src={databaseIcon}
+      />
+      <Image
+        position={[1200, 1200]}
+        ref={chatgpt}
+        height={600}
+        src={chatgptIcon}
+      />
 
-      <CodeBlock language="cpp" code={`for (int i = 1; i < 3; i++) {\n\tstd::cout << i << "\\n"; \n}`} />
+      <Node ref={questionContainer} position={[1200, 1200]}>
+        <JetbrainText ref={qMark1} scale={0} x={300} y={300} rotation={30} text={"?"} fontSize={300} />
+        <JetbrainText ref={qMark2} scale={0} x={-300} y={300} text={"?"} rotation={-30} fontSize={300} />
+        <JetbrainText
+          y={-250}
+          text={"Buatlah program yang menampilkan\nangka 1-3 dengan bahasa C++"}
+        />
+        <CodeBlock ref={wrongCode} language="cpp" code={``} />
+        <Layout
+          ref={outputContainer}
+          layout
+          direction={"column"}
+          y={300}
+          fontSize={100}
+        ></Layout>
+      </Node>
     </>
   );
   yield* database().scale(0, 0);
@@ -40,6 +81,22 @@ export default makeScene2D(function* (view) {
     chatgpt().position([0, 0], 1)
   );
   yield* waitFor(0.5);
-  yield* chatgpt().position([-1200, 1200], 1);
-  yield* waitFor(10);
+  yield* all(
+    chatgpt().position([-1200, 1200], 1),
+    questionContainer().position([0, 0], 1)
+  );
+  yield* wrongCode().edit(
+    1
+  )`${insert(`for (int i = 1; i < 3; i++) {\n\tstd::cout << i << "\\n"; \n}`)}`;
+  outputContainer().add(<JetbrainText text={"1"} />);
+  outputContainer().add(<JetbrainText text={"2"} />);
+  yield* waitFor(0.5)
+  yield* all(
+    qMark1().scale(1, 0.5).to(0, 0.8),
+    qMark2().scale(1, 0.5).to(0, 0.8)
+  );
+  yield* wrongCode().edit(1)`for (int i = 1; i ${edit(`<`, `<=`)} 3; i++) {\n\tstd::cout << i << "\\n"; \n}`;
+  yield* wrongCode().selection(lines(0, Infinity), 1);
+  outputContainer().add(<JetbrainText text={"3"} />);
+  yield* waitFor(0.8);
 });
